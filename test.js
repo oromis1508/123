a0baff8835df6c482a8d96d2c8841e0493ac5f98
a.chernik <a.chernik@a1qa.com>
2023-10-07 20:31:25 +0300

    asd

---
+++ test.js
@@ -0,0 +1,7 @@
+var12 = 123345;
+
+console.log(var12);
+var var12 = 3;
+var12 = 45;
+var12 = "asd";
+var12 = false;
---
+++ test.ts
@@ -0,0 +1,43 @@
+const var1: number = 100055555555555555555555555555444444444444444444444444444444444444444444444444444445555555555555000;
+// var1 = 45;
+let var2: number = 100007777777775555552222222222222222222222222222222222222222222222222222222222222222222222222225555555555555777700;
+// var2 = 456;
+var var3: number = 123;
+
+// console.log(var1);
+// console.log(var2);
+// console.log(var3);
+
+let str: string = "";
+let str2: string = "";
+let str3: string = "";
+
+let bool: boolean = false;
+
+let any1: any = false;
+any1 = "aasd";
+any1 = 123;
+
+let und;
+console.log(und);
+
+let symb: symbol = Symbol("123");
+let symb2 = Symbol("123");
+
+let int: BigInt = 2n;
+console.log(var1 * var2);
+let obj: object = {};
+obj = {};
+let arr = ["123", "14342", "345345"];
+var3 = NaN;
+
+console.log(typeof var1);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof any1);
+console.log(typeof und);
+console.log(typeof symb);
+console.log(typeof int);
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof var3);
